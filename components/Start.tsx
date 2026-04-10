9b02b786bbca4cd2b42f7c114c7b794db0de17f0
shogun444 <sayandiproy17@gmail.com>
2026-04-11 00:20:05 +0530

    Finished the component

--- components/Start.tsx
+++ components/Start.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { Subitle, Title } from "./FAQs";
+import { Answer, Subitle, Title } from "./FAQs";
 import { Button } from "./ui/button";
 import { Question } from "./Body";
 
@@ -18,43 +18,50 @@ export default function Start() {
 
       <Button>Start Free Trial</Button>
 
-      <div className="bg-neutral-200 rounded-2xl p-5 text-left gap-10 flex h-70 w-full">
-        <div> </div>
-        <div>
-          <Subitle>Navigation </Subitle>
-          <Label>Testimonials</Label>
-          <Label>FAQ</Label>
-          <Label>How it works</Label>
-          <Label>Pricing</Label>
-          <Label>Features</Label>
-          <Label>Changelog</Label>
-        </div>
-        <div>
-          <Subitle>Company </Subitle>
-          <Label>About</Label>
-          <Label>Framer templates</Label>
-          <Label>Privacy Policy</Label>
-          <Label>Terms and Condition</Label>
-          <Label>Contact Us</Label>
-          <Label>Blog</Label>
-        </div>
-
-        <div>
-          <Question className="text-neutral-400 font-bold ">
-            AI moves fast.{" "}
-          </Question>
-          <Question className="font-bold"> Stay ahead with Kresna.</Question>
-
-          <div className="relative my-4">
-            <input
-              type="text"
-              className="bg-white p-4 outline-none focus-visible:ring-1 focus-visible:ring-neutral-700 duration-200 w-80 font-semibold rounded-2xl text-[16px]"
-              placeholder="Enter email address"
-            />
-            <Button className="absolute text-md  tracking-normal py-6 shadow-input -translate-x-28 translate-y-1">
-              Subscribe
-            </Button>
+      <div className="flex-col my-40 bg-neutral-200 rounded-2xl p-5 text-left  w-200">
+        <div className="flex ">
+          <div>
+            <Subitle>Navigation </Subitle>
+            <Label>Testimonials</Label>
+            <Label>FAQ</Label>
+            <Label>How it works</Label>
+            <Label>Pricing</Label>
+            <Label>Features</Label>
+            <Label>Changelog</Label>
           </div>
+          <div className="ml-10">
+            <Subitle>Company </Subitle>
+            <Label>About</Label>
+            <Label>Framer templates</Label>
+            <Label>Privacy Policy</Label>
+            <Label>Terms and Condition</Label>
+            <Label>Contact Us</Label>
+            <Label>Blog</Label>
+          </div>
+        </div>
+
+        <div className="flex items-end">
+          <Answer className="tracking-tighter font-medium text-xs font-sans">
+            © 2025 Kresna. All rights reserved.
+          </Answer>
+          <div>
+            <div className="ml-50">
+              <Question className="text-neutral-400 font-bold ">
+                AI moves fast.
+              </Question>
+              <Question className="font-bold">Stay ahead with Kresna.</Question>
+              <div className="relative my-4">
+                <input
+                  type="text"
+                  className="bg-white p-4 outline-none focus-visible:ring-1 focus-visible:ring-neutral-700 duration-200 w-80 font-medium rounded-2xl text-[16px]"
+                  placeholder="Enter email address"
+                />
+                <Button className="absolute text-md  tracking-normal py-6 shadow-input -translate-x-28 translate-y-1">
+                  Subscribe
+                </Button>
+              </div>
+            </div>
+          </div>{" "}
         </div>
       </div>
     </div>
